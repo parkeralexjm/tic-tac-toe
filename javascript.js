11ae813981ce4d927df75d2c1e28bbe5f6cd1ea1
parkeralexjm <97406991+parkeralexjm@users.noreply.github.com>
2022-08-12 09:23:11 -0500

    Created board and display Added functions to create the game board and store the game state, styling for the board.

---
+++ javascript.js
@@ -0,0 +1,30 @@
+const newGame = (() => {
+  // Create an array to store the gamestate
+  var gameState = ["x", "x", "x", "o", "o", "o", "x", "o", "x"];
+  // Create a function that will create and display the board
+  const createBoard = () => {
+    var element = document.getElementById("boardContainer");
+    // Create 9 cells for the game board inside the boardContainer div
+    for (let i = 1; i <= 9; i++) {
+      var cell = document.createElement("div");
+      // Give each cell the class boardCell and an id from 1-9
+      cell.className = "boardCell";
+      cell.id = `${i}`;
+      element.appendChild(cell);
+    }
+  };
+  const updateState = () => {
+    for (let i = 1; i <= 10; i++) {
+      var element = document.getElementById(i);
+      const text = document.createTextNode(gameState[i - 1]);
+      element.appendChild(text);
+    }
+  };
+  return {
+    createBoard,
+    updateState
+  };
+})();
+
+newGame.createBoard();
+newGame.updateState();
