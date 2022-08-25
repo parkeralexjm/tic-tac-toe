9f2546ece9a92f2ed1d0dfe73e0b38317429f260
parkeralexjm <parkeralexjm@hotmail.co.uk>
2022-08-25 13:23:11 +0100

    Allow player to select symbol
    Added buttons to allow the player to select which symbols they would
    like to play with.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,18 @@
 
 
-const newGame = (() => {
+const gameAdmin = (() => {
   // Create an array to store the gamestate
   var gameState = ["", "", "", "", "", "", "", "", ""];
-  var playerSymbol = "o";
+  var playerSymbol = "x";
   // Create a function that will create and display the board
-  const createBoard = () => {
+  const createBoard = (playerSelection) => {
+    // Set default playerSelection
+    playerSelection = (typeof playerSelection !== 'undefined') ? playerSelection : "x";
+    // Clear any previous content from the game board and reset
     var element = document.getElementById("boardContainer");
+    element.innerHTML = "";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    playerSymbol = playerSelection;
     // Create 9 cells for the game board inside the boardContainer div
     for (let i = 1; i <= 9; i++) {
       var cell = document.createElement("div");
@@ -17,16 +23,20 @@ const newGame = (() => {
       // Add an event listener to each cell to add the player's symbol on click
       // then remove the listener
       cell.addEventListener('click', function AddSymbol() {
-        if (playerSymbol == "x") {
-          console.log(`cell ${i} is now x`)
-          gameState[i - 1] = "x";
-          this.removeEventListener('click', AddSymbol);
-          newGame.updateState();
-        } else { 
-          console.log(`cell ${i} is now o`);
-          gameState[i - 1] = "o";
-          this.removeEventListener('click', AddSymbol);
-          newGame.updateState();
+        if (gameState[i - 1] == ""){
+          if (playerSymbol == "x") {
+            console.log(`cell ${i} is now x`)
+            gameState[i - 1] = "x";
+            this.removeEventListener('click', AddSymbol);
+            playerSymbol = "o";
+            gameAdmin.updateState();
+          } else { 
+            console.log(`cell ${i} is now o`);
+            gameState[i - 1] = "o";
+            this.removeEventListener('click', AddSymbol);
+            playerSymbol = "x";
+            gameAdmin.updateState();
+          }
         }
       });
       element.appendChild(cell);
@@ -47,5 +57,5 @@ const newGame = (() => {
   };
 })();
 
-newGame.createBoard();
+gameAdmin.createBoard();
 //newGame.updateState();
