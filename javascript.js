44ca703d359fbbb9d77680a475983ca925837aa2
parkeralexjm <parkeralexjm@hotmail.co.uk>
2022-08-25 12:34:28 +0100

    Added click listener
    Added an event listener to each of the game cells that will change
    the symbol stored in gamestate and the update the board.

--- javascript.js
+++ javascript.js
@@ -1,37 +1,47 @@
+
+
 const newGame = (() => {
   // Create an array to store the gamestate
-  var gameState = ["x", "x", "x", "o", "o", "o", "x", "o", "x"];
+  var gameState = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
+  var playerSymbol = "o";
   // Create a function that will create and display the board
   const createBoard = () => {
     var element = document.getElementById("boardContainer");
     // Create 9 cells for the game board inside the boardContainer div
     for (let i = 1; i <= 9; i++) {
       var cell = document.createElement("div");
+      var pTag = document.createElement("p");
       // Give each cell the class boardCell and an id from 1-9
       cell.className = "boardCell";
-      cell.onclick = "newGame.reply_click(this.id)";
       cell.id = `${i}`;
+      cell.addEventListener('click', function() {
+        if (playerSymbol == "x") {
+          console.log(`cell ${i} is now x`)
+          gameState[i - 1] = "x";
+          newGame.updateState()
+        } else { 
+          console.log(`cell ${i} is now o`);
+          gameState[i - 1] = "o";
+          newGame.updateState()
+        }
+      });
       element.appendChild(cell);
+      cell.appendChild(pTag);
     }
   };
   const updateState = () => {
-    for (let i = 1; i <= 10; i++) {
-      var element = document.getElementById(i);
-      const tag = document.createElement("p");
-      const text = document.createTextNode(gameState[i - 1]);
-      tag.appendChild(text);
-      element.appendChild(tag);
+    for (let i = 1; i <= 9; i++) {
+      // For numbers 1-9 get the element, add a p tag and update it with the character
+      // stored in the gameState array that matches the id
+      var element = document.getElementById(i).firstChild;
+      element.innerText = gameState[i - 1];
     }
-  };
-  const reply_click = (clicked_id) => {
-    gameState[clicked_id] = "X";
   };
   return {
     createBoard,
-    updateState,
-    reply_click
+    updateState
   };
 })();
 
 newGame.createBoard();
-newGame.updateState();
+//newGame.updateState();
